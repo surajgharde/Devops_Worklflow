2e20f168f1c3db6e26c0e10021015a93a83776a7
Suraj Gharde <gharadesuraj@gmail.com>
2026-04-27 00:57:57 -0700

    test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD Pipeline Working."
+    return "CI/CD Pipeline testing."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000)
